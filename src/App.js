1e1903a67b6c36200d1526b084e7ddf99f3fe8b7
josedlca <jose.dlcruz.ayala@gmail.com>
2021-03-29 21:09:58 -0500

    first part init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div>
+        {/* left side */}
+        <div>
+
+          <div>
+            <h1>Title</h1>
+          </div>          
+
+          <div>
+            <h3>Mi Libreria</h3>
+            <ul>
+              <li>one</li>
+              <li>one</li>
+              <li>one</li>
+              <li>one</li>
+              <li>one</li>
+            </ul>
+          </div>
+
+        </div>
+        {/* left side end*/}
+
+        {/* results side*/}
+        <div>
+          <div>
+
+            {/* search side */}            
+            <div>
+              <div>
+                <input type="text"/>
+                <div>Buscar</div>
+              </div>
+
+              <div>
+                <div>Logo</div>
+                <div>Usuario</div>
+              </div>
+            </div>
+            {/* search side */}
+
+            {/* video jumbotron */}
+            <div>
+              <div>
+                <div>VIDEO</div>
+              </div>
+              <div>
+                <div>Name artist</div>
+                <div>
+                  <div>Lo mejor de adele</div>
+                  <div>seguidores</div>
+                </div>
+                <div>
+                  <button>Reproducir</button>
+                  <button>Seguir</button>
+                </div>
+              </div>
+            </div>
+            {/* video jumbotron */}
+
+            {/* Resultados */}
+            <div>
+              <div>Resultados</div>
+              <div>
+
+                <div>
+                  <div>
+                    img
+                  </div>
+                  <div>
+                    <div>name song</div>
+                    <div>name artist</div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            {/* Resultados end*/}
+          </div>
+        </div>
+        {/* results side end*/}
+        <div></div>
+      </div>
+    </main>
   );
 }
 
